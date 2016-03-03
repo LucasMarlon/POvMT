4835d40729538b2c0e05ade1f0a5d098d3f3a9b8
Rayssa <rayssaledo@gmail.com>
2016-03-02 22:02:23 -0300

    Incluindo o tempo investido em Atividade

--- ProjetoPiloto/app/src/main/java/povmt/projeto/les/povmt/projetopiloto/models/Atividade.java
+++ ProjetoPiloto/app/src/main/java/povmt/projeto/les/povmt/projetopiloto/models/Atividade.java
@@ -1,22 +1,26 @@
 package povmt.projeto.les.povmt.projetopiloto.models;
 
-/**Classe para objetos do tipo Atividade, onde serão contidos, valores e métodos para o mesmo.
+/**Classe para objetos do tipo Atividade, onde serao contidos, valores e metodos para o mesmo.
  * @author Marcos Nascimento
  * @version 1.00
- * @since Release 01 da aplicação
+ * @since Release 01 da aplicacao
  */
 public class Atividade {
 
     private long Id;
     private String nome;
+    private int tempoInvestido;
 
     /**
-     * O construtor Atividade sem argumento não inicializa nenhuma variável.
+     * O construtor Atividade com argumentos nome e tempoIvestido.
      */
-    public Atividade(){    }
+    public Atividade(String nome, int tempoInvestido){
+        this.nome = nome;
+        this.tempoInvestido = tempoInvestido;
+    }
 
     /**
-     * Obtém o Id.
+     * Obtï¿½m o Id.
      * @return um <code>long</code> especificando a Atividade.
      */
     public long getId() {
@@ -26,14 +30,14 @@ public class Atividade {
     /**
      * Configura o nome.
      * @param nome o nome
-     * @throws Exception no caso do nome ser inválido.
+     * @throws Exception no caso do nome ser invalido.
      */
     public void setNome(String nome) throws Exception {
         if(nome == null){
-            throw new Exception("Atributo nome não pode ser null.");
+            throw new Exception("Atributo nome nao pode ser null.");
         }
         else if(nome.equals("")){
-            throw new Exception("Nome não pode ser a string vazia.");
+            throw new Exception("Nome nao pode ser a string vazia.");
 
         }
         else{
@@ -42,9 +46,29 @@ public class Atividade {
     }
 
     /**
-     * Obtém o nome.
+     * Obtem o nome.
      * @return <code>String</code> nomeando a Atividade.*/
     public String getNome() {
         return nome;
+    }
+
+    /**
+     * Obtem o tempo investido na atividade
+     * @return int atribuindo tempo a atividade
+     */
+    public int getTempo_investido() {
+        return tempoInvestido;
+    }
+
+    /**
+     * Modifica o tempo investido na atividade
+     * @param tempoInvestido
+     */
+    public void setTempoInvestido(int tempoInvestido) throws Exception {
+        if (tempoInvestido < 0) {
+            throw new Exception("Tempo nÃ£o pode ser negativo.");
+        } else {
+            this.tempoInvestido = tempoInvestido;
+        }
     }
 }
