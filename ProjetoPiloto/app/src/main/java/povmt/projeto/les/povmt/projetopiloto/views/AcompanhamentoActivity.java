764442d719c1ccc414acb10caccab811364894b7
Lucas Marlon <lucas.marques@ccc.ufcg.edu.br>
2016-03-06 18:52:15 -0300

    Interface gráfica de acompanhamento

--- ProjetoPiloto/app/src/main/java/povmt/projeto/les/povmt/projetopiloto/views/AcompanhamentoActivity.java
+++ ProjetoPiloto/app/src/main/java/povmt/projeto/les/povmt/projetopiloto/views/AcompanhamentoActivity.java
@@ -1,22 +1,13 @@
 package povmt.projeto.les.povmt.projetopiloto.views;
 
-import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
-
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 
 import java.util.ArrayList;
 
@@ -24,114 +15,38 @@ import povmt.projeto.les.povmt.projetopiloto.R;
 
 public class AcompanhamentoActivity extends ActionBarActivity {
 
-    private RelativeLayout acompanhamentoLayout;
-    private PieChart mChart;
-    // we're going to display pie chart for smartphones martket shares
-    private float[] yData = { 5, 10, 15, 30, 40 };
-    private String[] xData = { "Sony", "Huawei", "LG", "Apple", "Samsung" };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acompanhamento);
 
-        acompanhamentoLayout = (RelativeLayout) findViewById(R.id.acompanhamentoLayout);
-        mChart = new PieChart(this);
-        // add pie chart to main layout
-        acompanhamentoLayout.addView(mChart);
-        acompanhamentoLayout.setBackgroundColor(Color.parseColor("#55656C"));
+        //Criando um array com as Proporções de TI
+        ArrayList<BarEntry> entradas = new ArrayList<>();
+        entradas.add(new BarEntry(10f, 4)); //O primeiro parâmetro do objeto BarEntry é um valor float e o sengundo refere-se a posição
+        entradas.add(new BarEntry(8f, 3));
+        entradas.add(new BarEntry(6f, 2));
+        entradas.add(new BarEntry(3f, 1));
+        entradas.add(new BarEntry(1f, 0));
 
-        // configure pie chart
-        mChart.setUsePercentValues(true);
-        mChart.setDescription("Smartphones Market Share");
+        BarDataSet dataset = new BarDataSet(entradas, "Proporção de TI (%)");
 
-        // enable hole and configure
-        mChart.setDrawHoleEnabled(true);
-        mChart.setHoleColorTransparent(true);
-        mChart.setHoleRadius(7);
-        mChart.setTransparentCircleRadius(10);
+        //Criando um array com o nome das atividades
+        ArrayList<String> nomeDeAtividades = new ArrayList<String>();
+        nomeDeAtividades.add("Atividade 1");
+        nomeDeAtividades.add("Atividade 2");
+        nomeDeAtividades.add("Atividade 3");
+        nomeDeAtividades.add("Atividade 4");
+        nomeDeAtividades.add("Atividade 5");
 
-        // enable rotation of the chart by touch
-        mChart.setRotationAngle(0);
-        mChart.setRotationEnabled(true);
+        HorizontalBarChart  chart = new HorizontalBarChart (this);
+        setContentView(chart);
 
-        // set a chart value selected listener
-        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
-            @Override
-            public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
-                // display msg when value selected
-                if (e == null)
-                    return;
-                Toast.makeText(AcompanhamentoActivity.this,
-                        xData[e.getXIndex()] + " = " + e.getVal() + "%", Toast.LENGTH_SHORT).show();
-            }
-            @Override
-            public void onNothingSelected() {
+        BarData data = new BarData(nomeDeAtividades, dataset);
+        chart.setData(data);
 
-            }
-        });
-
-        // add data
-        addData();
-
-        // customize legends
-        Legend l = mChart.getLegend();
-        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
-        l.setXEntrySpace(7);
-        l.setYEntrySpace(5);
+        chart.setDescription("Total de TI: ");
     }
 
-    private void addData() {
-        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
-
-        for (int i = 0; i < yData.length; i++)
-            yVals1.add(new Entry(yData[i], i));
-
-        ArrayList<String> xVals = new ArrayList<String>();
-
-        for (int i = 0; i < xData.length; i++)
-            xVals.add(xData[i]);
-
-        // create pie data set
-        PieDataSet dataSet = new PieDataSet(yVals1, "Market Share");
-        dataSet.setSliceSpace(3);
-        dataSet.setSelectionShift(5);
-
-        // add many colors
-        ArrayList<Integer> colors = new ArrayList<Integer>();
-
-        for (int c : ColorTemplate.VORDIPLOM_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.JOYFUL_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.COLORFUL_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.LIBERTY_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.PASTEL_COLORS)
-            colors.add(c);
-
-        colors.add(ColorTemplate.getHoloBlue());
-        dataSet.setColors(colors);
-
-        // instantiate pie data object now
-        PieData data = new PieData(xVals, dataSet);
-        data.setValueFormatter(new PercentFormatter());
-        data.setValueTextSize(11f);
-        data.setValueTextColor(Color.GRAY);
-
-        mChart.setData(data);
-
-        // undo all highlights
-        mChart.highlightValues(null);
-
-        // update pie chart
-        mChart.invalidate();
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
