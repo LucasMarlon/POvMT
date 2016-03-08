bc6fcb031bee749c6e9d0535ff4ee3d85d29b745
unknown <andreza.queiroz@ccc.ufcg.edu.br>
2016-03-08 20:55:29 -0300

    arquivos de conexão com o mongo - requisições post

---
+++ conf_mongo/models/atividade.js
@@ -0,0 +1,195 @@
+module.exports = function(mongodb, app, atividadeCollection) {
+	app.post("/cadastrarAtividade", function (req, res){
+		var nomeAtividade = req.body.nomeAtividade;
+		var dataInicioSemana = req.body.dataInicioSemana;
+		var dataFimSemana = req.body.dataFimSemana;
+		var prioridade = req.body.prioridade;
+		var	foto = req.body.foto;
+		var	categoria = req.body.categoria;
+		var dataAtividade = req.body.dataAtividade;
+		
+		atividadeCollection.find({
+			nomeAtividade: nomeAtividade,
+			dataInicioSemana: dataInicioSemana
+			
+		}).toArray(function(err, array){
+			if(err) {
+				res.send('{ "ok" : 0, "msg" : "' + err + '" }');
+			} else {
+				if(array.length > 0) {
+					res.send("ATIVIDADE EXISTENTE");
+				} else {
+					atividadeCollection.insert({
+						nomeAtividade: nomeAtividade,
+						dataInicioSemana: dataInicioSemana,
+						dataFimSemana: dataFimSemana,
+						prioridade: prioridade,
+						foto: foto,
+						categoria: categoria,
+						dataAtividade: dataAtividade,						
+						tempoInvestido : 0
+					
+					},function(err, doc){
+						if(err){
+							res.send('{ "ok" : 0, "msg" : "' + err + '" }');
+						} else {
+							res.send('"ok": 1');	
+						}
+					});
+				}				
+			}
+		});			
+	});
+	
+	app.post("/incrementaTempoInvestido", function (req, res){
+		var dataInicioSemana = req.body.dataInicioSemana;
+		var nomeAtividade = req.body.nomeAtividade;
+		var tempoInvestido = req.body.tempoInvestido;
+		
+		atividadeCollection.find({
+			nomeAtividade: nomeAtividade,
+			dataInicioSemana: dataInicioSemana
+			
+		}).toArray(function(err, array){
+			if(err) {
+				res.send('{ "ok" : 0, "msg" : "' + err + '" }');
+			} else {
+				if(array.length == 0) {
+					res.send("ATIVIDADE INEXISTENTE");
+				} else {
+					atividadeCollection.update({
+						nomeAtividade:nomeAtividade, 
+						dataInicioSemana: dataInicioSemana
+					},
+					{ $inc: { tempoInvestido: parseInt(tempoInvestido) } 
+					
+					},function(err, doc){
+						if(err){
+							res.send('{ "ok" : 0, "msg" : "' + err + '" }');
+						} else {
+							res.send('"ok": 1');	
+						}
+					});
+				}				
+			}
+		});
+		
+	});
+	
+	app.post("/updateAtividade", function (req, res){
+		var nomeAtividade = req.body.nomeAtividade
+		var dataInicioSemana = req.body.dataInicioSemana;
+		var dataFimSemana = req.body.dataFimSemana;
+		var prioridade = req.body.prioridade;
+		var	foto = req.body.foto;
+		var	categoria = req.body.categoria;
+		var dataAtividade = req.body.dataAtividade;
+		var tempoInvestido = req.body.tempoInvestido;
+		
+		atividadeCollection.find({
+			nomeAtividade: nomeAtividade,
+			dataInicioSemana: dataInicioSemana
+			
+		}).toArray(function(err, array){
+			if(err) {
+				res.send('{ "ok" : 0, "msg" : "' + err + '" }');
+			} else {
+				if(array.length != 0) {
+					res.send("ATIVIDADE INEXISTENTE");
+				} else {
+					atividadeCollection.update({
+						nomeAtividade:nomeAtividade, 
+						dataInicioSemana: dataInicioSemana
+					},
+					{ 
+						nomeAtividade: nomeAtividade,
+						dataInicioSemana: dataInicioSemana,
+						dataFimSemana: dataFimSemana,
+						prioridade: prioridade,
+						foto: foto,
+						categoria: categoria,
+						dataAtividade: dataAtividade,						
+						tempoInvestido : parseInt(tempoInvestido) 
+					
+					},function(err, doc){
+						if(err){
+							res.send('{ "ok" : 0, "msg" : "' + err + '" }');
+						} else {
+							res.send('{ "ok" : 1 }');	
+						}
+					});
+				}				
+			}
+		});
+		
+	});
+	
+	app.post("/deleteAtividade", function (req, res){
+		var nomeAtividade = req.body.nomeAtividade;
+		var dataInicioSemana = req.body.dataInicioSemana;
+		
+		atividadeCollection.find({
+			nomeAtividade: nomeAtividade,
+			dataInicioSemana: dataInicioSemana
+			
+		}).toArray(function(err, array){
+			if(err) {
+				res.send('{ "ok" : 0, "msg" : "' + err + '" }');
+			} else {
+				if(array.length == 0) {
+					res.send("ATIVIDADE INEXISTENTE");
+				} else {
+					atividadeCollection.remove({
+						nomeAtividade: nomeAtividade,
+						dataInicioSemana: dataInicioSemana
+					}, function(err, doc){
+						if(err){
+							res.send('{ "ok" : 0, "msg" : "' + err + '" }');
+						} else {
+							res.send('{ "ok" : 1 }');	
+						}
+					});
+				}				
+			}
+		});
+		
+	});
+	
+	
+	app.post("/findAtividade", function (req, res){
+		var nomeAtividade = req.body.nomeAtividade;
+		var dataInicioSemana = req.body.dataInicioSemana;
+		
+		atividadeCollection.findOne({
+			nomeAtividade: nomeAtividade,
+			dataInicioSemana: dataInicioSemana
+			
+		}, function(err, doc){
+			if(err){
+				res.send('{ "ok" : 0, "msg" : "' + err + '" }');
+			} else {
+				res.json(doc);	
+			}
+		});
+		
+	});
+	
+	app.post("/findAtividadesSemana", function(req, res){
+		var dataInicioSemana = req.body.dataInicioSemana;
+
+		atividadeCollection.find({
+			
+			dataInicioSemana: dataInicioSemana
+			
+		}).toArray(function(err, array){
+			if(err) {
+				res.send('{ "ok" : 0, "msg" : "' + err + '" }');
+			} else {
+				res.send(array);		
+			}
+		});	
+		
+	});
+	
+	return this;
+}
---
+++ conf_mongo/server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var app = express();
+var mongo = require("mongodb");
+var bodyParser = require("body-parser");
+ 
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+ 
+new mongo.Db("povmt", new mongo.Server(process.env.OPENSHIFT_MONGODB_DB_HOST || "localhost", process.env.OPENSHIFT_MONGODB_DB_PORT || 27017)).open(function(err, client){
+    if (err) {
+        console.log("ERRO: " + err);
+    } else {
+        client.authenticate(process.env.OPENSHIFT_MONGODB_DB_USERNAME, process.env.OPENSHIFT_MONGODB_DB_PASSWORD, function(err) {
+            if (err) {
+                console.log("ERRO DE AUTENTICAÇÃO: " + err);
+            } else {
+                console.log("SUCESSO!");
+                client.collection("atividade", function(err, collection){
+                    if (err){
+                        console.log("ERRO: " + err);
+                    } else {
+						require("./models/atividade")(mongo, app, collection);
+                    }
+                });
+            }
+        });
+    }
+});
+ 
+app.get("/", function(req, res) {
+    res.send("Hello world");
+});
+ 
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || "localhost");
